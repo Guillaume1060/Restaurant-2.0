51051266f624191605749d79a548508929e5bf4f
guillaume1060 <breyerguillaume@gmail.com>
2022-07-04 16:53:59 +0200

    creation backoffice.php

---
+++ backOffice.php
@@ -0,0 +1,116 @@
+<?php
+///CONNEXION TO THE DATABASE
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "";
+	$db = "restaurant";
+
+	$bdd = new PDO("mysql:host=$dbhost;dbname=$db",$dbuser,$dbpass);
+	
+    try {
+        $bdd;
+    } catch (Exception $e) {
+        die('erreur: '.$e->getMessage());
+    }
+
+/// CATCH DATAS FROM DATABASE
+$requete = $bdd->query('SELECT * FROM clients');
+
+
+/// UPDATE THE DATABASE WHEN A NEW MESSAGE IS SENT
+if (!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['email'])){
+    
+    $firstName                  = $_POST['firstname'];
+    $lastName                   = $_POST['lastname'];
+    $email                      = $_POST['email'];
+    $type                       = $_POST['type']; 
+    $comments                   = $_POST['comments'];
+
+
+    /// si la condition est remplie (champ OK) on créé la requete, qu'on execute ensuite
+    $ajoutViaFormulaire = $bdd->prepare('INSERT INTO clients(firstname, lastname, email, type, comments) 
+                            VALUES(?, ?, ?, ?, ?)') 
+                            or die(print_r($bdd->errorInfo()));
+
+    $ajoutViaFormulaire->execute(array($firstName, $lastName, $email, $type, $comments));
+
+    ///  Permet de ne pas multiplier les ajouts (rafraichir la page)
+    // header('location: ../localhost/Restaurant-2.0/backOffice.php');
+
+}
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="styles.css" />
+    <title>Da Rosy - Back Office</title>
+</head>
+
+  <header>
+  </header>
+<main>
+
+<h1>BACK OFFICE MANAGEMENT</h1>
+<!-- DISPLAY AN ARRAY -->
+    <table id="background_table">
+        <tr>
+            <th>Id</th>
+            <th>Date</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Type</th>
+            <th>Comments</th>
+            <th>Delete</th>
+        </tr>
+
+<!-- Loop for each columns -->
+<?php
+while ($donnees = $requete->fetch()){
+echo '  <tr>
+                <td>'.$donnees['id'].'</td>
+                <td>'.$donnees['date'].'</td>
+                <td>'.$donnees['firstname'].'</td>
+                <td>'.$donnees['lastname'].'</td>
+                <td>'.$donnees['email'].'</td>
+                <td>'.$donnees['type'].'</td>
+                <td>'.$donnees['comments'].'</td>
+                <td>Click to delete</td>
+        </tr>';
+}
+
+?>
+    </table>
+
+</main>
+<footer class="bg-light opacity-75 text-center fixed-bottom">
+  <a href="#up"><span class="bi bi-chevron-up text-dark"></span></a>
+  <h6 class="text-dark">© 2022 Copyright - Breyer</h6>
+</footer>
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
